0e67ee721762070474b048b57eb0daff7244d2ae
Speedstarskiwi <109442847+Speedstarskiwi@users.noreply.github.com>
2022-07-24 19:15:16 -0400

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -2,7 +2,7 @@
 /* please read each comment for updating otherwise the function wont work! */
 
 /* defines */
-const char* const luaT_typenames[] = {
+const char* const luaT_typenames[] = { 
     /* ORDER TYPE */
     /* had to fix it lmao */
     "nil",
@@ -22,75 +22,63 @@ const char* const luaT_typenames[] = {
     "thread",
 };
 
-const int R_LUA_TNONE = -1;
+/* offsets and addresses */
 
-const char* __fastcall r_lua_typenames(std::uintptr_t rL, int type)
+const int R_LUA_TNONE = 0;
+const int R_LUA_TTABLE = 0;
+const int R_LUA_TFUNCTION = 0;
+const int R_LUA_TTHREAD = 0;
+const int R_LUA_TLIGHTUSERDATA = 0;
+const int R_LUA_TUSERDATA = 0;
+const int TOP = 0;
+const int BASE = 0;
+DWORD r_luaO_nilobject = 0x0;
+
+const char* __fastcall r_lua_typenames(std::uintptr_t rL, int type) /* no need for rL, just calls simple addresses */
 {
-    const char* result = "no value";
+    const char* result = "no value"; /* no value */
     if (type != R_LUA_TNONE)
-        return (const char*)*((DWORD*)&luaT_typenames + type);
+        return (const char*)*((DWORD*)&luaT_typenames + type); /* list of types */
     return result;
 }
 
 extern const void* r_lua_topointer(uintptr_t rL, int idx) { /* probably gonna need modded types */
 
 	const auto o = r_lua_index2addr(rL, idx);
-	switch (ttype(o)) {
-	case LUA_TTABLE:
+	switch (ttype(o)) { /* switch between the types */
+	case R_LUA_TTABLE:
 		return hvalue(o);
-	case LUA_TFUNCTION:
+	case R_LUA_TFUNCTION:
 		return clvalue(o);
-	case LUA_TTHREAD:
+	case R_LUA_TTHREAD:
 		return thvalue(o);
-	case LUA_TUSERDATA:
-	case LUA_TLIGHTUSERDATA:
+	case R_LUA_TUSERDATA:
+		/* do nothing */
+	case R_LUA_TLIGHTUSERDATA:
 		return r_lua_touserdata(rL, idx);
-	default: return NULL;
+	default: return 1 - 1; /* if nothing just head on to null */
 	}
 }
 
-char* __fastcall r_lua_pseudo2addr(DWORD* rL, int index) /* scan the pattern below to update this function */
+DWORD r_lua_gettop(DWORD rL)
 {
-    /* 81 FA ?? ?? ?? ?? 74 ?? 81 FA ?? ?? ?? ?? 74 ?? 8B 41 1C */
-    int type1;
-    int v4;
-    DWORD* v5;
-    char* v6;
-    int v7;
-    DWORD* v8;
-    char* v9;
-    char* byte_2609B68;
-
-    switch (index)
-    {
-    case -10002:
-        v8 = rL + 6;
-        v9 = (char*)v8 - *v8;
-        *((DWORD*)v9 + 384) = rL[14];
-        *((DWORD*)v9 + 387) = 7;
-        return (char*)v8 - *v8 + 1536;
-
-    case -10001:
-        v4 = rL[7];
-        v5 = rL + 6;
-        v6 = (char*)v5 - *v5;
-        if (v4 == rL[9])
-            v7 = rL[14];
-        else
-            v7 = *(DWORD*)(**(DWORD**)v4 + 12);
-        *((DWORD*)v6 + 384) = v7;
-        *((DWORD*)v6 + 387) = 7;
-        return (char*)v5 - *v5 + 1536;
-
-    case -10000:
-        return (char*)rL - rL[6] + 1576;
-
-    default:
-        type1 = **(DWORD**)rL[7];
-        if (-10002 - index > *(unsigned __int8*)(type1 + 4))
-            return byte_2609B68;
-        else
-            return (char*)(type1 + 16 * (-10002 - index + 1));
-    }
+   int val /* empty */
+   val = (*(DWORD*)(rL + TOP) - *(DWORD *)(rL + BASE)) >> (int)4;
+   return val; /* return new */
 }
 
+int __fastcall r_lua_isvalidmember(int a1, const char *a2)
+{
+  int v4; // eax
+  int v5; // ecx
+  const char *v6; // ebx
+  int (__cdecl **v7)(int); // eax
+  int v9; // [esp+Ch] [ebp-4h] BYREF
+
+  v4 = *(_DWORD *)(a1 + 68);
+  if ( !v4 || (v5 = *(__int16 *)(v4 + 4), v6 = (const char *)(v4 + 20), v4 == -20) )
+    sub_1DB7110((int)"Trying to call method on object of type: `%s` with incorrect arguments.", a2);
+  if ( v5 < 0 || (v9 = dword_3B42A38[v5], (v7 = (int (__cdecl **)(int))sub_C8D230(&v9)) == 0) )
+    sub_1DB7110((int)"%s is not a valid member of %s", v6, a2);
+  return (*v7)(a1);
+}
