e9be9d8efc805d782e5d66ee54130809151eb226
Speedstarskiwi <109442847+Speedstarskiwi@users.noreply.github.com>
2022-07-24 19:27:31 -0400

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -67,18 +67,22 @@ DWORD r_lua_gettop(DWORD rL)
    return val; /* return new */
 }
 
-int __fastcall r_lua_isvalidmember(int a1, const char *a2)
-{
-  int v4; // eax
-  int v5; // ecx
-  const char *v6; // ebx
-  int (__cdecl **v7)(int); // eax
-  int v9; // [esp+Ch] [ebp-4h] BYREF
+int __thiscall unknownshit(int *this, unsigned int *a2) { int v2; int v3; unsigned int v5;unsigned int v6; int v7;unsigned int v8;  DWORD *v9; unsigned int v10; int v11; v2 = this[1]; v3 = *this; v11 = *this; if ( *this == v2 ) return 0; v5 = *a2; v12 = this[4]; if ( v5 == v12 ) { LABEL_8: v9 = 0; } else { v6 = ((v2 - v3) >> 3) - 1; v7 = v6 & (v5 + (v5 >> 3)); v8 = 0; while ( 1 ) { v9 = (_DWORD *)(v3 + 8 * v7); if ( *v9 == v5 ) break; if ( *v9 != v12 ) { v3 = v11; v10 = v8 + v7 + 1; ++v8; v7 = v6 & v10; if ( v8 <= v6 ) continue; } goto LABEL_8; } } return v9 != 0 ? (unsigned int)(v9 + 1) : 0; }
 
-  v4 = *(_DWORD *)(a1 + 68);
-  if ( !v4 || (v5 = *(__int16 *)(v4 + 4), v6 = (const char *)(v4 + 20), v4 == -20) )
-    sub_1DB7110((int)"Trying to call method on object of type: `%s` with incorrect arguments.", a2);
-  if ( v5 < 0 || (v9 = dword_3B42A38[v5], (v7 = (int (__cdecl **)(int))sub_C8D230(&v9)) == 0) )
-    sub_1DB7110((int)"%s is not a valid member of %s", v6, a2);
-  return (*v7)(a1);
+int __fastcall r_lua_isvalidmember(int a1, const char* a2) /* custom function that checks if valid member i suggest you replace this whole function and scan the AOB below */
+{
+        int dword_3B42A38[605]
+	int v4;
+	int v5;
+	const char* v6;
+	int(__cdecl * *v7)(int);
+	int v9;
+
+	v4 = *(_DWORD*)(a1 + 68);
+	if (!v4 || (v5 = *(__int16*)(v4 + 4), v6 = (const char*)(v4 + 20), v4 == -20))
+		std::cout << "Wrong arguments performed" << std::endl;
+	if (v5 < 0 || (v9 = dword_3B42A38[v5], (v7 = (int(__cdecl**)(int))unknownshit(&v9)) == 0))
+		std::cout << v6 << " is not a valid member of " << a2 << std::endl;
+	return (*v7)(a1);
+	/* 55 8B EC 83 E4 ?? 51 53 56 57 8B F9 8B F2 8B 47 44 */
 }
